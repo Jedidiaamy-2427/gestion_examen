24763814a111ef58831c991033d2869fcbae2c40
Jedidiaamy-2427 <jedidiaamyandrianjafi@gmail.com>
2025-10-03 17:26:06 +0300

    feat(exam): Add exam list component

--- frontend/src/app/components/exam/exam.component.ts
+++ frontend/src/app/components/exam/exam.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ExamService } from '../../core/services/exam.service';
 
 @Component({
   selector: 'app-exam',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './exam.component.html',
   styleUrl: './exam.component.css'
 })
 export class ExamComponent {
+
+  constructor(protected ExamService: ExamService, private router: Router){}
+
+  ngOnInit() {
+    this.ExamService.getAll().subscribe()
+  }
 
 }
